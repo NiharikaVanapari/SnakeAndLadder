5fe6412e517031e6df76db5c8f9994e23ec79140
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-22 23:04:58 +0530

    Initial Commit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,6 @@ public class SnakeAndLadder
 public static void main(String[] args)
 {
 System.out.println("Welcome to Snake And Ladder");
-int PlayerPosition=0;
-System.out.println("Player Position is : "+PlayerPosition);
 }
 }
 
