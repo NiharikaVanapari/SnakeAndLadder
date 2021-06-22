9624b65b7c4288f85d447849350c0186dad15b67
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-23 00:01:34 +0530

    OptionCheck

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class SnakeAndLadder
 {
 public static void main(String[] args)
 {
-System.out.println("Welcome to Snake And Ladder");
 int PlayerPosition=1;
-System.out.println("Player position is : "+PlayerPosition);
+
+System.out.println("Welcome to Snake And Ladder");
 int die = (int) (Math.floor( Math.random() * 10) % 6+1);
 System.out.println("Die rolled is :" +die);
+int option =(int) (Math.floor(Math.random() * 10)%3);
+switch(option)
+{
+case 0:
+System.out.println("No play");
+PlayerPosition += 0;
+break;
+case 1:
+System.out.println("Ladder");
+PlayerPosition += die;
+break;
+default:
+System.out.println("Snake");
+PlayerPosition -= die;
+if(PlayerPosition<0)
+PlayerPosition=0;
+}
+System.out.println("Player position is : "+PlayerPosition);
 }
 }
 
