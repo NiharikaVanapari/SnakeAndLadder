75dea64d87d9ae76da487f5e21980cd427a67087
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-22 23:26:22 +0530

    Dice roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,9 +3,10 @@ public class SnakeAndLadder
 public static void main(String[] args)
 {
 System.out.println("Welcome to Snake And Ladder");
-int PlayerPosition=0;
+int PlayerPosition=1;
 System.out.println("Player position is : "+PlayerPosition);
-
+int die = (int) (Math.floor( Math.random() * 10) % 6+1);
+System.out.println("Die rolled is :" +die);
 }
 }
 
